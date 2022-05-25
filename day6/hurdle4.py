275ef71ce53e6b91fa7c52feb794582ef92e1a7f
Mike Davis <mike.davis@psyvad.com>
2022-05-25 11:51:22 -0600

    complete day 6

---
+++ day6/hurdle4.py
@@ -0,0 +1,21 @@
+# def turnRight():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump_hurdle():
+#     turn_left()
+#     while wall_on_right() == True:
+#         move()
+#     turnRight()
+#     move()
+#     turnRight()
+#     while front_is_clear() == True:
+#         move()
+#     turn_left()
+
+# while at_goal() != True:
+#     if wall_in_front() == True:
+#         jump_hurdle()
+#     elif front_is_clear() == True:
+#         move()
---
+++ day6/project6/escapeMaze.py
@@ -0,0 +1,16 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_on_right() == False:
+#         turn_right()
+#         move()
+#     elif wall_in_front() == True:
+#         if wall_on_right() == True:
+#             turn_left()
+#         else:
+#             turn_right()
+#     else:
+#         move()
